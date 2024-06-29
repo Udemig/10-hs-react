f637fca9d4239d8aee8ca57da57aba04e42b33df
furkanevin <furkanevin00@gmail.com>
2024-06-29 22:06:18 +0300

    ice cream app

--- 5_unit_test/2_ice_cream_app/src/components/Card/card.test.jsx
+++ 5_unit_test/2_ice_cream_app/src/components/Card/card.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Card from ".";
+import userEvent from "@testing-library/user-event";
 
+// prop olarak gönderilcek örnek item
 const item = {
   name: "Chocolate",
   imagePath: "/images/chocolate.png",
@@ -35,6 +37,55 @@ test("Miktar,başlık ve fotoğraf gelen propa göre ekrana basılır", () => {
   expect(image).toHaveAttribute("src", "/images/chocolate.png");
 });
 
-// todo azalt butonunu aktiflik testleri
-
 // ekle azalt butonlarında çalışıcak fonksiyonların testleri
+test("Butonlara tıklanınca fonksiyonlar doğru parametre ile çalışır", async () => {
+  const user = userEvent.setup();
+
+  // prop olarak gönderilen fonksiyonu test ediceksek jest aracılığı ile test (mock) ediliebilir fonksiyonlar oluştur
+  const addMockFn = jest.fn();
+  const removeMockFn = jest.fn();
+
+  render(
+    <Card
+      item={item}
+      amount={5}
+      addToBasket={addMockFn}
+      removeFromBasket={removeMockFn}
+    />
+  );
+
+  // butonları al
+  const addBtn = screen.getByRole("button", { name: /ekle/i });
+  const delBtn = screen.getByRole("button", { name: /azalt/i });
+
+  // ekle butonuna tıkla
+  await user.click(addBtn);
+
+  // addToBasket methodu doğru parametreler ile çağrıldı mı
+  expect(addMockFn).toHaveBeenCalledWith(item);
+
+  // azalt butonuna tıkla
+  await user.click(delBtn);
+
+  // removeFromBasket methodu doğru parametrelerle çalışıyor mu
+  expect(removeMockFn).toHaveBeenCalledWith(item.id);
+});
+
+// todo azalt butonunu aktiflik testleri
+describe("azalt butonu akitflik testleri", () => {
+  it("miktar 1 den fazla ise buton aktiftir", () => {
+    render(<Card item={item} amount={3} />);
+
+    const button = screen.getByRole("button", { name: "Azalt" });
+
+    expect(button).toBeEnabled();
+  });
+
+  it("miktar 0 ise buton inaktiftir", () => {
+    render(<Card item={item} amount={0} />);
+
+    const button = screen.getByRole("button", { name: "Azalt" });
+
+    expect(button).toBeDisabled();
+  });
+});
